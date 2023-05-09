4eaf56b67f97641ed2d7d541bb9f582b4431bb8c
Mariusz <heniek0316@gmail.com>
2023-05-09 20:06:55 +0200

    first

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-let name = 'Mariusz'
-let age = 40
-let favorite = 'meat'
+const name = 'Mariusz'
+const petName = 'Kluska'
 
-console.log(name,age,favorite)
+console.log(`Czesc, jestem ${name}, a to jest ${petName} - mój kot`)
+
+const agePet = 15
+
+console.log(petName + ' ma już ' + agePet + ' lat');
+
